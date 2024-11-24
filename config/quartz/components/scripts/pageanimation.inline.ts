00e04a383960ac290ff369a9704fa0e7b2363742
David Varghese <dvdmtw99@gmail.com>
2024-11-23 21:00:07 -0600

    Vault Backup: 2024-11-23 21:00

--- config/quartz/components/scripts/pageanimation.inline.ts
+++ config/quartz/components/scripts/pageanimation.inline.ts
@@ -1,5 +1,5 @@
 document.addEventListener("nav", () => {
-    const IGNORED_CLASSES = ['popover', 'popover-inner', 'popover-hint', 'backlinks'];
+    const targetElement = '#quartz-body'
 
     // Animation to apply to elements
     function animateElement(target: HTMLElement, delay: number): void {
@@ -40,46 +40,21 @@ document.addEventListener("nav", () => {
         });
     }
 
-    // Helper function to check if an element or its ancestors have ignored classes
-    function hasIgnoredClass(node: Node): boolean {
-        if (node instanceof HTMLElement) {
-            return IGNORED_CLASSES.some((className) => node.classList.contains(className));
-        }
-        return false;
-    }
-
     // Callback function for the MutationObserver
     const handleContentChange: MutationCallback = (
         mutations: MutationRecord[],
         observer: MutationObserver
     ): void => {
-        const contentContainer = document.querySelector('#quartz-body') as HTMLElement;
-
-        // Filter mutations to exclude ignored classes
-        const isRelevantChange = mutations.some((mutation) => {
-            // Check if the target or its ancestors should be ignored
-            let currentNode: Node | null = mutation.target;
-            while (currentNode && currentNode !== contentContainer) {
-                if (hasIgnoredClass(currentNode)) {
-                    return false;
-                }
-                currentNode = currentNode.parentNode;
-            }
-
-            return true;
-        });
-
-        if (contentContainer && isRelevantChange) {
-            applyStaggeredAnimation(contentContainer);;
-        }
+        const contentContainer = document.querySelector(targetElement) as HTMLElement;
+        applyStaggeredAnimation(contentContainer);
     };
 
     const observer = new MutationObserver(handleContentChange);
 
     // Select the dynamic content container and start observing it
-    const contentContainer = document.querySelector('#quartz-body') as HTMLElement;
+    const contentContainer = document.querySelector(targetElement) as HTMLElement;
     if (contentContainer) {
-        observer.observe(contentContainer, { characterData: true, childList: true, subtree: true });
+        observer.observe(contentContainer, { characterData: true, childList: true, subtree: false });
         // Required for the animation to work on the 404 page
         applyStaggeredAnimation(contentContainer);
     }
